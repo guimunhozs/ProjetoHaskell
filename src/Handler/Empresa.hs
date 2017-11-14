e210dd9ba40149b03e5fa73bff14dedc42b6c674
romefeller <luisgustabo@uol.com.br>
2017-11-14 03:07:59 +0000

    put de empresa

--- src/Handler/Empresa.hs
+++ src/Handler/Empresa.hs
@@ -15,3 +15,10 @@ getEmpresaR :: Handler TypedContent
 getEmpresaR = do
     empresa <- (runDB $ selectList [] [])::Handler [Entity Empresa]
     sendStatusJSON created201 $ object["empresa".= empresa]
+
+putEmpresaIdR :: EmpresaId -> Handler Value
+putEmpresaIdR empresaId = do
+    _ <- runDB $ get404 empresaId
+    novaEmpresa <- requireJsonBody :: Handler Empresa
+    runDB $ replace empresaId novaEmpresa
+    sendStatusJSON noContent204 (object ["resp" .= ("UPDATED " ++ show (fromSqlKey empresaId))])

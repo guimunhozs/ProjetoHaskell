d1444cb92290427f561c1736e2d90d22405979f9
romefeller <luisgustabo@uol.com.br>
2017-11-15 21:11:19 +0000

    POST de Cliente

--- src/Handler/Cliente.hs
+++ src/Handler/Cliente.hs
@@ -16,4 +16,8 @@ getClienteR = do
     cliente <- (runDB $ selectList [] [])::Handler [Entity Cliente]
     sendStatusJSON created201 $ object["cliente".= cliente]
 
-
+--curl -X POST -v https://haskalpha-romefeller.c9users.io/cliente -d '{"nome" : "carrara" , "rg" :  "45454545" , "cpf" : "12345678910" , "logradouro" :"Rua do tcc pronto 10" , "bairro": "Entrega" , "cep" : "11123456" , "telefone" :"13999999999" , "email" : "agustinho@carrara.com.br" , "excluido" : false, "cidadeId" :1, "estadoId" : 1 }'postClienteR :: Handler TypedContent
+postClienteR = do
+    cliente <- (requireJsonBody :: Handler Cliente)
+    clienteId <- runDB $ insert cliente
+    sendStatusJSON created201 $ object["clienteId" .= clienteId]

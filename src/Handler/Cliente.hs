60a3340ef7f69ab6fb0868b1b054bd18666f40e4
romefeller <luisgustabo@uol.com.br>
2017-11-17 15:55:05 +0000

    Get de clientes ordenado por nome e filtrando excluidos

--- src/Handler/Cliente.hs
+++ src/Handler/Cliente.hs
@@ -11,10 +11,17 @@ import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 --curl -v -X Get https://haskalpha-romefeller.c9users.io/cliente
+{-
 getClienteR :: Handler TypedContent
 getClienteR = do
     cliente <- (runDB $ selectList [] [])::Handler [Entity Cliente]
-    sendStatusJSON created201 $ object["cliente".= cliente]
+    sendStatusJSON created201 $ object["cliente".= cliente] 
+-}
+
+getClienteR :: Handler TypedContent
+getClienteR = do
+    cliente <- (runDB $ selectList [ClienteExcluido ==. False][Asc ClienteNome]) :: Handler [Entity Cliente]
+    sendStatusJSON created201 $ object["Cliente" .= cliente]
 
 --curl -X POST -v https://haskalpha-romefeller.c9users.io/cliente -d '{"nome" : "carrara" , "rg" :  "45454545" , "cpf" : "12345678910" , "logradouro" :"Rua do tcc pronto 10" , "bairro": "Entrega" , "cep" : "11123456" , "telefone" :"13999999999" , "email" : "agustinho@carrara.com.br" , "excluido" : false, "cidadeId" :1, "estadoId" : 1 }'postClienteR :: Handler TypedContent
 postClienteR :: Handler TypedContent
@@ -47,6 +54,6 @@ getClienteIdR :: ClienteId -> Handler TypedContent
 getClienteIdR clienteId = do
     runDB $ do 
         cliente <- get404 clienteId
-        cidade <- get404 $ clienteCidadeId cliente
-        estado <- get404 $ clienteEstadoId cliente
+        cidade <- get404 $ clienteCidadeId $  cliente
+        estado <- get404 $ clienteEstadoId $  cliente
         sendStatusJSON ok200 $ object ["Cliente" .= cliente, "Cidade" .= cidade, "Estado" .= estado]

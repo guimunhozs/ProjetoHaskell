d4fbb77b9c32e0d07e1b28f92f793cc74c30cdff
romefeller <luisgustabo@uol.com.br>
2017-11-17 16:20:20 +0000

    post de dados bancarios

--- src/Handler/DadosBancarios.hs
+++ src/Handler/DadosBancarios.hs
@@ -10,7 +10,14 @@ import Import
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
+--curl -v -X Get https://haskalpha-romefeller.c9users.io/dadosbancarios
 getDadosBancariosR :: Handler TypedContent
 getDadosBancariosR = do
     dadosbancarios <- (runDB $ selectList [] [Asc DadosBancariosNome]) :: Handler [Entity DadosBancarios]
     sendStatusJSON created201 $ object["DadosBancarios" .= dadosbancarios]
+
+postDadosBancariosR :: Handler Value
+postDadosBancariosR = do
+    dadosbancarios <- (requireJsonBody :: Handler DadosBancarios)
+    dadosbancariosId <- runDB $ insert dadosbancarios
+    sendStatusJSON created201 $ object["DadosBancariosId" .= dadosbancariosId]

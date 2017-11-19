aac1730b24ea46ad9c088d27f9632226b016313b
romefeller <luisgustabo@uol.com.br>
2017-11-19 20:13:28 +0000

    get de conta a receber e a pagar separado

--- src/Handler/Conta.hs
+++ src/Handler/Conta.hs
@@ -10,9 +10,14 @@ import Import
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
-getContaR :: Handler TypedContent
-getContaR = do
-    contas <- (runDB $ selectList [][]) :: Handler [Entity Conta]
+getContasPagarR :: Handler TypedContent
+getContasPagarR = do
+    contas <- (runDB $ selectList [ContaIcPagarReceber ==. True][Desc ContaDataVencimento]) :: Handler [Entity Conta]
+    sendStatusJSON created201 $ object["Conta" .= contas]
+
+getContasReceberR :: Handler TypedContent
+getContasReceberR = do
+    contas <- (runDB $ selectList [ContaIcPagarReceber ==. False][Desc ContaDataVencimento]) :: Handler [Entity Conta]
     sendStatusJSON created201 $ object["Conta" .= contas]
 
 postContaR :: Handler TypedContent
@@ -20,4 +25,3 @@ postContaR = do
     conta <- requireJsonBody :: Handler Conta
     contaId <- runDB $ insert conta
     sendStatusJSON created201 $ object["contaId" .= contaId]
-    

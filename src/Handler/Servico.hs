8c18ba54fb8a95cbe75ed3c13273cd13baa184b8
romefeller <guilhermemunhozsilva@outlook.com>
2017-11-27 01:08:51 +0000

    ajustes GET Servico e GET servico ID

--- src/Handler/Servico.hs
+++ src/Handler/Servico.hs
@@ -18,11 +18,16 @@ postServicoR = do
     
 getServicoR :: Handler TypedContent
 getServicoR = do
-    servicos <- (runDB $ selectList [] [])::Handler [Entity Servico]
-    sendStatusJSON created201 $ object["servicos".= servicos]
+    result <- runDB $ do
+        servico <- selectList [] []
+        equip   <- mapM (get . servicoEquipamentoId . entityVal) servico
+        cliente <- mapM (get . equipamentoClienteId) (catMaybes equip)
+        return $ zip3 servico (catMaybes equip)  (catMaybes cliente) 
+    sendStatusJSON ok200 $ object ["result" .= result]
+    --testar
     
-pathServicoIdStatusR :: ServicoId -> Int -> Handler Value
-pathServicoIdStatusR sid status = do 
+patchServicoIdStatusR :: ServicoId -> Int -> Handler Value
+patchServicoIdStatusR sid status = do 
     _ <- runDB $ get404 sid
     runDB $ update sid [ServicoStatus =. status]
     sendStatusJSON noContent204 (object ["resp" .= (fromSqlKey sid)])
@@ -36,8 +41,14 @@ putServicoIdR servicoId = do
     
 getServicoIdR :: ServicoId -> Handler TypedContent
 getServicoIdR servicoId = do
-    servico <- runDB $ get404 servicoId
-    sendStatusJSON created201 $ object["servico".= servico]
+    (serv, equip, cli, andaF) <- runDB $ do
+        serv  <- get404 servicoId
+        equip <- get404 $ servicoEquipamentoId serv
+        cli   <- get404 $ equipamentoClienteId equip
+        anda  <- selectList [AndamentoServicoId ==. servicoId] []
+        func  <- mapM (get404 . andamentoFuncionarioId . entityVal) anda
+        return (serv, equip, cli, zip anda func)
+    sendStatusJSON ok200 $ object ["Servico" .= serv, "Equipamento" .= equip, "Cliente" .= cli, "Andamento" .= andaF]
     
     
 --https://www.stackage.org/haddock/lts-9.12/yesod-core-1.4.37/Yesod-Core-Handler.html#v:lookupSession
